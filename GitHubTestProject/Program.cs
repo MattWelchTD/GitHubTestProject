8abb3866f1af21161b3f971d8024589eb6bf583a
MattWelchTD <mattw@UKPC14047.office.lan>
2015-11-03 11:02:11 +0000

    Changed program.cs as a test

--- GitHubTestProject/Program.cs
+++ GitHubTestProject/Program.cs
@@ -7,6 +7,7 @@ namespace GitHubTestProject
 {
     static class Program
     {
+        /// another comment line here 
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
